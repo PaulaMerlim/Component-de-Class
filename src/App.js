329f5b8c9f0aa914a28fe47dd79653f5cf8403d7
Paulinha Merlim <104771318+PaulaMerlim@users.noreply.github.com>
2022-07-20 12:52:12 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Faça login no CodeSandbox
+
+// Crie um novo sandbox em REACT
+
+// Crie um Component de class que retorne uma frase na tela
+
+import React, { Component } from "react";
+class App extends Component {
+  render() {
+    return <h1> Estou me esforçando ao máximo, mas está difícil.</h1>;
+  }
+}
+export default App;
